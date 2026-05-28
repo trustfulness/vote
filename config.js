08fe06010c92b9966b0475e5389189a3656e6ac5
trustfulness <trustfulness2026@gmail.com>
2026-05-28 21:57:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste your Google Apps Script Web App URL after deployment (ends with /exec)
 window.ENROLL_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzf1o0deUvYK0F0eFWCnAb3N5DPa098YIXIQHO-VCopmPBrmG81sbLpT2q_u39xv19N/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxjF0WMMR2SteH0L00qzbgVig49_zPCwp_fGTx9hGX1MOENCqReKKwhZDxRdAeiVgu2/exec",
   refreshInterval: 10000,  // 10 seconds in milliseconds
 };
